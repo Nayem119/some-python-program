c9c29bbcc1421c53a5244f67fd417af4f9538e87
Nayem Ahammad <nayemahammad3@gmail.com>
2026-07-23 20:42:07 +0600

    ...........

--- ​৪. লজিক্যাল গেম ও টুলস/৩২. ডাইস রোলার (সিমুলেশন) লুপ দিয়ে ১-৬ এর মধ্যে র‍্যান্ডম সংখ্যা প্রিন্ট।.py
+++
@@ -1,4 +0,0 @@
-#  ডাইস রোলার (সিমুলেশন): লুপ দিয়ে ১-৬ এর মধ্যে র‍্যান্ডম সংখ্যা প্রিন্ট।
-import random
-for i in range(10):
-    print(random.randint(1, 6)) 
--- ​৪. লজিক্যাল গেম ও টুলস/​৩১. নাম্বার গেসিং গেম কম্পিউটার একটি সংখ্যা ধরবে, ইউজারকে guess করতে হবে।.py
+++
@@ -1,8 +0,0 @@
-# ​৩১. নাম্বার গেসিং গেম: কম্পিউটার একটি সংখ্যা ধরবে, ইউজারকে guess করতে হবে।
-import random
-computer_number=random.randint(1, 100)
-user_number=int(input("Enter a number: "))
-if user_number==computer_number:
-    print("You win")
-else:
-    print("You lose")
